79d2dc76e8d1c401febd99a7cc6b51522f3b659a
Aakash Jagwani <aakashjagwani09@gmail.com>
2017-12-15 23:07:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,30 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require('express');   //A fast web application framework that works on the fly.
+var bodyParser = require('body-parser'); 
 var request = require('request');
-var app = express();
+var app = express(); 
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({extended: false})); 
 app.use(bodyParser.json());
-app.listen((process.env.PORT || 3000));
+app.listen((process.env.PORT || 3000));  //Port no to listen on local server.
 
-// GET to check if server is up and running
+// GET request to check if server is up and running
 app.get('/', function (req, res) {
     res.send('This is TestBot Server');
 });
 
-// GET for Facebook Webhook
+// GET request for testing  the Facebook Webhook
 app.get("/webhook", function (req, res) {
-  if (req.query["hub.verify_token"] === process.env.VERIFICATION_TOKEN) {
-    console.log("Verified webhook");
-    res.status(200).send(req.query["hub.challenge"]);
+  if (req.query["hub.verify_token"] === "Any_String_will_work") {   //random String to be tested with facebook verify_token 
+    console.log("Verified webhook"); 
+    res.status(200).send(req.query["hub.challenge"]);  //should be same in the Facebooks webhook API verification token #i Wasted 2 hours After this..
   }  else {
     console.error("Verification failed. The tokens do not match.");
     res.sendStatus(403);
 
     }
 });
+
+//We can handle different messages by using different apis and handle large number of customers.
 // handler receiving messages
 app.post('/webhook', function (req, res) {
     var events = req.body.entry[0].messaging;
@@ -37,8 +39,8 @@ app.post('/webhook', function (req, res) {
 // generic function sending messages
 function sendMessage(recipientId, message) {
     request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
+        url: 'https://graph.facebook.com/v2.6/me/messages',  //URL which generates messages from the server. 
+        qs: {access_token: process.env.PAGE_ACCESS_TOKEN},  //Access token generated from FB`s API by creating a page and should be put here. 
         method: 'POST',
         json: {
             recipient: {id: recipientId},
